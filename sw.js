bac12132bb439ddda1f2369019f1bb6904e9117c
widarba3 <widarbasaritiga@gmail.com>
2026-02-05 13:33:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
+const CACHE_NAME = 'bankflow-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
