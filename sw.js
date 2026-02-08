f494222af66ef71fdf77210ce42d26b9b1c4ea6e
widarba3 <widarbasaritiga@gmail.com>
2026-02-08 11:29:18 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,76 +1,24 @@
-// Service Worker untuk JIMPITAN MANG RESTI PWA
-const CACHE_NAME = 'jimpitan-cache-v1';
-
-// FILE YANG AKAN DI-CACHE
-const filesToCache = [
-  '/bankflow-jimpitan/',
-  '/bankflow-jimpitan/index.html',
-  '/bankflow-jimpitan/manifest.json',
-  '/bankflow-jimpitan/favicon.ico',
-  '/bankflow-jimpitan/favicon-16x16.png',
-  '/bankflow-jimpitan/favicon-32x32.png',
-  '/bankflow-jimpitan/apple-touch-icon.png',
-  '/bankflow-jimpitan/android-chrome-192x192.png',
-  '/bankflow-jimpitan/android-chrome-512x512.png'
+// Service Worker untuk PWA
+const CACHE_NAME = 'jimpitan-v2.0';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/shimmer-effects.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://html2canvas.hertzen.com/dist/html2canvas.min.js'
 ];
 
-// PASANG SERVICE WORKER
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-      })
-      .then(() => self.skipWaiting())
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-// AKTIFKAN SERVICE WORKER
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-    .then(() => self.clients.claim())
-  );
-});
-
-// TANGANI PERMINTAAN
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        
-        return fetch(event.request)
-          .then(response => {
-            if (!response || response.status !== 200) {
-              return response;
-            }
-            
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-            
-            return response;
-          })
-          .catch(() => {
-            if (event.request.mode === 'navigate') {
-              return caches.match('/bankflow-jimpitan/index.html');
-            }
-          });
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
